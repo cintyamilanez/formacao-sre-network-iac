39699e50152ddd8d1729685a0972209ac23dae1c
Cintya Milanez <cintya_milanez@hotmail.com>
2023-10-29 16:04:25 -0300

    Formatando tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ module "vpc" {
   name = "${var.vpc_name}-vpc"
   cidr = var.vpc_cidr
 
-  azs            = var.subnet_azs
-  public_subnets = var.public_subnets
+  azs             = var.subnet_azs
+  public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 
-  public_subnet_names = ["${var.vpc_name}-public-subnet"]
+  public_subnet_names  = ["${var.vpc_name}-public-subnet"]
   private_subnet_names = ["${var.vpc_name}-private-subnet"]
 
   vpc_tags = {
